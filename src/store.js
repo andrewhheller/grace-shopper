02afe67661be561554295f016d69df625da78545
Andrew Heller <andrewhheller@gmail.com>
2018-10-25 15:40:55 -0400

    imported product reducer file in main store file

--- src/store.js
+++ src/store.js
@@ -1,7 +1,9 @@
 import {createStore, applyMiddleware, combineReducers} from 'redux';
 import loggerMiddleware from 'redux-logger';
 import thunk from 'redux-thunk';
+
 import UserReducer from './reducers/UserReducer';
+import { productReducer } from './reducers/products';
 
 const reducer = combineReducers({
     users: UserReducer,
