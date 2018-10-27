09cbc49b9dc8c4a0331f8597e4197415bfc03db6
slieberman1126 <slieberman1126@gmail.com>
2018-10-27 16:56:45 -0400

    minor changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,7 @@
 import React, { Component, Fragment } from 'react';
-import { getUsers, exchangeTokenForAuth } from '../store'
+import { getUsers, exchangeTokenForAuth } from '../store';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
-import { connect } from 'react-redux'
-import Users from './Users';
+import { connect } from 'react-redux';
 import Nav from './Nav';
 import Users from './Users';
 import User from './User';
@@ -13,38 +12,45 @@ import Login from './Login';
 import RegisterUser from './RegisterUser';
 import AdminManagement from './AdminManagement';
 
-
 class App extends Component {
-    componentDidMount(){
-        this.props.getUsers()
-        this.props.exchangeTokenForAuth()
-    }
+  componentDidMount() {
+    this.props.getUsers();
+    this.props.exchangeTokenForAuth();
+  }
 
-    render() {
-        console.log(this.props.history)
-        return (
-            <Router>
-                <Fragment>
-                    <Route path='/' render={({history}) => <Nav history={history} />} />   
-                    <Switch>
-                        <Route exact path='/users' component={Users} />
-                        <Route path='/users/:id' component={User} />
-                        <Route path='/login' render={({history}) => <Login history={history} />} />
-                        <Route path='/register' component={ RegisterUser } />
-                        <Route path='/adminManagement' component={ AdminManagement } />
-                        <Route path='/' component={ Home } />
-                    </Switch> 
-                </Fragment>
-            </Router>
-        )
-    }
+  render() {
+    console.log(this.props.history);
+    return (
+      <Router>
+        <Fragment>
+          <Route path="/" render={({ history }) => <Nav history={history} />} />
+          <Switch>
+            <Route exact path="/users" component={Users} />
+            <Route path="/users/:id" component={User} />
+            <Route
+              path="/login"
+              render={({ history }) => <Login history={history} />}
+            />
+            <Route path="/register" component={RegisterUser} />
+            <Route path="/adminManagement" component={AdminManagement} />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/products" component={Products} />
+            <Route path="/products/:id" component={ProductDetails} />
+          </Switch>
+        </Fragment>
+      </Router>
+    );
+  }
 }
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        getUsers: () => dispatch(getUsers()),
-        exchangeTokenForAuth: () => dispatch(exchangeTokenForAuth())
-    }
-}
+const mapDispatchToProps = dispatch => {
+  return {
+    getUsers: () => dispatch(getUsers()),
+    exchangeTokenForAuth: () => dispatch(exchangeTokenForAuth()),
+  };
+};
 
-export default connect(null, mapDispatchToProps)(App)
+export default connect(
+  null,
+  mapDispatchToProps
+)(App);
--- src/reducers/UserReducer.js
+++ src/reducers/UserReducer.js
@@ -1,69 +1,66 @@
 import {
-    GET_USERS,
-    DELETE_USER,
-    UPDATE_USER,
-    ADD_USER,
-    _deleteUser,
-    _updateUser,
-    _addUser,
-    _getUsers } from './UserActions';
+  GET_USERS,
+  DELETE_USER,
+  UPDATE_USER,
+  ADD_USER,
+  _deleteUser,
+  _updateUser,
+  _addUser,
+  _getUsers,
+} from './UserActions';
 import axios from 'axios';
 
-const updateUser = (user) => {
-    return (dispatch) => {
-        return axios.put(`/api/users/${user.id}`, user)
-            .then(res => res.data)
-            .then(user => dispatch(_updateUser(user)))
-    };
+const updateUser = user => {
+  return dispatch => {
+    return axios
+      .put(`/api/users/${user.id}`, user)
+      .then(res => res.data)
+      .then(user => dispatch(_updateUser(user)));
+  };
 };
 
-const addUser = (user) => {
-    return (dispatch) => {
-        return axios.post(`/api/users`, user)
-            .then(res => res.data)
-            .then(user => dispatch(_addUser(user)))
-    };
+const addUser = user => {
+  return dispatch => {
+    return axios
+      .post(`/api/users`, user)
+      .then(res => res.data)
+      .then(user => dispatch(_addUser(user)));
+  };
 };
 
 const deleteUser = (user, history) => {
-    return (dispatch) => {
-        dispatch(_deleteUser(user));
-        return axios.delete(`/api/user/${user.id}`)
-            .then(()=> history.goBack());
-    };
+  return dispatch => {
+    dispatch(_deleteUser(user));
+    return axios.delete(`/api/user/${user.id}`).then(() => history.goBack());
+  };
 };
 
 const getUsers = () => {
-    return (dispatch) => {
-        return axios.get('/api/users')
-            .then(res => res.data)
-            .then(users => dispatch(_getUsers(users)))
-    };
+  return dispatch => {
+    return axios
+      .get('/api/users')
+      .then(res => res.data)
+      .then(users => dispatch(_getUsers(users)));
+  };
 };
 
 const getUserById = (users, id) => users.find(user => user.id === id);
 
 const UserReducer = (state = [], action) => {
-    switch(action.type){
-        case GET_USERS:
-            return action.users;
-        case DELETE_USER:
-            return state.filter(user => user.id != action.user.id);
-        case UPDATE_USER:
-            return state.map(user => {
-                return user.id === action.user.id ? action.user : user;
-            })
-        case ADD_USER:
-            return [ ...state, action.user];     
-        default: return state
-    };
+  switch (action.type) {
+    case GET_USERS:
+      return action.users;
+    case DELETE_USER:
+      return state.filter(user => user.id != action.user.id);
+    case UPDATE_USER:
+      return state.map(user => {
+        return user.id === action.user.id ? action.user : user;
+      });
+    case ADD_USER:
+      return [...state, action.user];
+    default:
+      return state;
+  }
 };
 
-export { 
-    getUsers,
-    updateUser,
-    addUser,
-    deleteUser,
-    getUserById,
-    UserReducer
-}
+export { getUsers, updateUser, addUser, deleteUser, getUserById, UserReducer };
