27d2af1d860383051d6dac9283916e33905ef5e7
slieberman1126 <slieberman1126@gmail.com>
2018-10-27 16:56:45 -0400

    minor changes

--- src/components/Products.js
+++ src/components/Products.js
@@ -4,22 +4,20 @@ import { connect } from 'react-redux';
 import ProductListing from './ProductListing';
 
 const Products = ({ products }) => {
-
   return (
     <div>
       <h2>All Products:</h2>
-      {
-        products.map(product => <ProductListing key={ product.id }product={ product } />)
-      }
-     </div>
-  )
-
+      {products.map(product => (
+        <ProductListing key={product.id} product={product} />
+      ))}
+    </div>
+  );
 };
 
 const mapStateToProps = ({ products }) => {
-    return {
-      products
-    };
+  return {
+    products,
+  };
 };
 
 export default connect(mapStateToProps)(Products);
