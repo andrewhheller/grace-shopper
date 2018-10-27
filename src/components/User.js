52df5e7d2b6d8cac2faabc76c7ecd75b3388c2bb
Andrew Heller <andrewhheller@gmail.com>
2018-10-27 14:43:29 -0400

    added main user UI page

--- src/components/User.js
+++ src/components/User.js
@@ -1,26 +1,116 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import {getUserById} from './../reducers/UserReducer';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Grid , List, ListItem, ListItemLink, ListItemText} from '@material-ui/core'
+
+import { getUserById } from './../reducers/UserReducer';
+
+import UserInfo from './UserInfo';
+import UserOrders from './UserOrders';
+
 
 class User extends Component {
 
+    constructor() {
+        super();
+        this.state = {
+            userArea: '' // determines which Component is rendered in main area to the right
+        }
+
+        this.handleUserArea = this.handleUserArea.bind(this);
+        this.selected = this.selected.bind(this);
+    }
+
+    // componentDidUpdate(prevProps) {
+    //     const { location } = this.props;
+    //     const path = location.pathname;
+    //     const userArea = path.substr(path.lastIndexOf('/') + 1, path.length);
+
+    //     if(prevProps !== this.props) {
+    //         this.setState({ userArea: 'UserInfo' })
+    //     }
+    // }
+
+    // returns Component to render in main area (to the right) when user area button is clicked
+    handleUserArea(userArea) {
+        const { user } = this.props;
+            
+        switch(userArea) {
+            case 'UserInfo':
+                return <UserInfo user={ user }/>
+
+            case 'UserOrders':
+                return <UserOrders />
+        }
+
+    }
+
+    // active button styling
+    selected(currentPath) {
+        const { location } = this.props;
+
+        if(location.pathname.includes(currentPath)) {
+            return true;
+        }
+
+        return false;
+    }
+
     render() {
-        const {user} = this.props;
+        const { handleUserArea, selected } = this;
+        const { user } = this.props;
+        const { userArea } = this.state;
+
+        const style = {
+            GridItem: { padding: 10, marginTop: 10, height: "90vh" }
+        }
+        
+        if(!user){
+            return null;
+        }
+
         return (
-            <div className='userContainer'>
-                <div >
-                    {user.firstName} {user.lastName} {user.email} {user.userName}
-                </div>
-            </div>
+
+            <Grid container spacing={24}>
+                <Grid item sm={2} style={style.GridItem}>
+                    <List>
+                    
+                        <ListItem
+                            button onClick={ () => this.setState({ userArea: 'UserInfo' }) }
+                            component={ Link } 
+                            to={`/users/${ user.id }/acount-info`}
+                            selected={ selected('/acount-info') }
+                        >
+                            <ListItemText>My Account</ListItemText>
+                        </ListItem>
+
+                        <ListItem
+                            button onClick={ () => this.setState({ userArea: 'UserOrders' }) }
+                            component={ Link } 
+                            to={`/users/${ user.id }/orders`}
+                            selected={ selected('/orders') }
+                        >
+                            <ListItemText>My Orders</ListItemText>
+                        </ListItem>
+                    
+                    </List>
+                </Grid>
+                <Grid item sm style={style.GridItem}> 
+                    { handleUserArea(userArea) } 
+                </Grid>
+            </Grid>
+
         )
     }
 };
 
-const mapStateToProps = ({users}, {match}) => {
+const mapStateToProps = ({ users }, { match }) => {
     const id = parseInt(match.params.id)
+
     return {
-        user: getUserById(users, id),
+        user: getUserById(users, id)
     }
 };
 
 export default connect(mapStateToProps)(User);
+
