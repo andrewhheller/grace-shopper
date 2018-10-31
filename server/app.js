cefddb0a046cb7b960da1245990bd6ceb4be2e98
Andrew Heller <andrewhheller@gmail.com>
2018-10-31 14:13:13 -0400

    added body parser URLencoded to POST data

--- server/app.js
+++ server/app.js
@@ -1,18 +1,27 @@
 const path = require('path');
+const bodyParser = require('body-parser')
 const express = require('express');
 const app = express();
 
 module.exports = app;
 
-app.use(express.json());
+// body parser
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// static resources
 app.use("/public", express.static(path.join(__dirname, '..', 'public')));
 app.use(express.static(path.join(__dirname, '..', 'dist')));
+
+// api middleware
 app.use("/api", require('./api'));
 
+// main routes
 app.get('/', (req, res, next) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 })
 
+// error handling
 app.use((err, req, res, next) => {
     console.error(err, typeof next)
     console.error(err.stack)
